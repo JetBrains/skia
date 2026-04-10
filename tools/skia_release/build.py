2210cc7a482c2009e56f82d198474979c0645bef
Hubert Błaszczyk <hubertblaszczyk03@gmail.com>
2026-04-10 11:13:32 +0200

    Enable Graphite Dawn for Android

--- tools/skia_release/build.py
+++ tools/skia_release/build.py
@@ -169,6 +169,8 @@ def main():
           'is_trivial_abi=false',
       ]
   elif target == 'android':
+    if enable_graphite_dawn:
+      args += ['dawn_enable_vulkan=true']
     args += [
         'ndk="' + ndk + '"',
         'skia_use_vulkan=true',
