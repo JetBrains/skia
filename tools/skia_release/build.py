2cf7502f5b4fb0cde88e0e60c6e2f294a3e33820
Dustin Feucht <dustin.feucht@jetbrains.com>
2026-09-07 18:26:23 +0200

    Set WebAssembly stack size to 1MB in skia_release build configuration

--- tools/skia_release/build.py
+++ tools/skia_release/build.py
@@ -230,7 +230,7 @@ def main():
         'skia_wasm_sdk="' + wasi_sdk + '"',
         'extra_cflags+=["--target=wasm32-wasip1", "-flto=thin", "--sysroot=' + sysroot + '", "-I' + gl_headers + '", "-I' + egl_headers + '", "-mllvm", "-wasm-enable-sjlj", "-mexception-handling", "-D_WASI_EMULATED_MMAN", "-D_WASI_EMULATED_SIGNAL", "-D_WASI_EMULATED_PROCESS_CLOCKS", "-D_WASI_EMULATED_GETPID", "-DU_HAVE_TZSET=0", "-DU_HAVE_TIMEZONE=0", "-DU_HAVE_TZNAME=0"]',
         'extra_cflags_cc+=["--target=wasm32-wasip1", "--sysroot=' + sysroot + '", "-I' + gl_headers + '", "-I' + egl_headers + '", "-mllvm", "-wasm-enable-sjlj", "-mexception-handling", "-D_WASI_EMULATED_MMAN", "-D_WASI_EMULATED_SIGNAL", "-D_WASI_EMULATED_PROCESS_CLOCKS", "-D_WASI_EMULATED_GETPID", "-DU_HAVE_TZSET=0", "-DU_HAVE_TIMEZONE=0", "-DU_HAVE_TZNAME=0"]',
-        'extra_ldflags+=["--target=wasm32-wasip1", "-flto=thin", "-Wl,--gc-sections", "-Wl,--strip-all", "--sysroot=' + sysroot + '", "-lsetjmp", "-lwasi-emulated-mman", "-lwasi-emulated-signal", "-lwasi-emulated-process-clocks", "-lwasi-emulated-getpid", "-mllvm", "-wasm-enable-sjlj", "-mexception-handling"]',
+        'extra_ldflags+=["--target=wasm32-wasip1", "-flto=thin", "-Wl,--gc-sections", "-Wl,--strip-all", "--sysroot=' + sysroot + '", "-lsetjmp", "-lwasi-emulated-mman", "-lwasi-emulated-signal", "-lwasi-emulated-process-clocks", "-lwasi-emulated-getpid", "-mllvm", "-wasm-enable-sjlj", "-mexception-handling", "-Wl,-z,stack-size=1048576"]',
     ]
 
   if gpu_as_extension:
